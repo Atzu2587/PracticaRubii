671a55b985a6369600757f7e2dd65bda00ba7131
Atzu2587 <nicolas.salinas.salazar@gmail.com>
2022-05-26 12:44:54 -0400

    agregando variables ruby

---
+++ variable.rb
@@ -0,0 +1,17 @@
+#sumando dos variables
+x=10
+y=20
+suma= x + y
+puts suma
+#multiplicando do variables
+precio_dolar= 20
+peso_cl= 100
+
+conversion = precio_dolar * peso_cl
+puts conversion
+#Variables tipo texto
+texto_saludo="hola desde chile"
+puts texto_saludo
+puts "hasta el proximo codigo chaval"
+
+
