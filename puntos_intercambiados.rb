ba8b96d85e295f4ca674cc4f788673c657acf628
Atzu2587 <nicolas.salinas.salazar@gmail.com>
2022-06-08 10:43:40 -0400

    imprimir par

--- puntos_intercambiados.rb
+++ puntos_intercambiados.rb
@@ -3,11 +3,19 @@ print "ingresa cuantos numeros quieres imprimir: "
 n= gets.chomp.to_i
 
 (1..n).each do |var_block|
-
+#validar si es para usando el metodo even
     if var_block.even?
         print var_block
     else
         print "."
     end
 
+
+    #validar si es para usando el operador de modulo
+    #if var_block%2 == 0
+    #    print var_block
+    #else
+    #    print "."
+    #end
 end
+
