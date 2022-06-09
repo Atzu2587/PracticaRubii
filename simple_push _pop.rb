9bffd192195857fdcc8ddbb635412c5778b3093c
Atzu2587 <nicolas.salinas.salazar@gmail.com>
2022-06-09 11:53:30 -0400

    arreglos pop

---
+++ simple_push _pop.rb
@@ -0,0 +1,10 @@
+arreglo1=[1,2,4,5]
+arreglo1.push(6)
+print arreglo1
+arreglo1.pop
+print arreglo1
+listaPlatillos=['enchiladas','mole','tostadas','tacos']
+listaPlatillos.push ('Mixiotes')
+print listaPlatillos
+listaPlatillos.pop
+print listaPlatillos
