d923e03079bc01453fb8fdd1a3bbab178d54b7de
Atzu2587 <nicolas.salinas.salazar@gmail.com>
2022-06-09 10:35:30 -0400

    method array

---
+++ simple_method_array.rb
@@ -0,0 +1,17 @@
+#Declarando arreglos 
+arregloInt = [4923,32,745,3232,89,2,1,898,777,2]
+arregloString = ["Miguel","Paula","Juana","Maria","Natalia","Cesar"]
+#Operaciones con arreglos
+puts arregloInt.size
+puts arregloString.length
+print arregloInt.reverse
+puts ""
+print arregloString.reverse
+puts ""
+print arregloInt.shuffle
+puts ""
+print arregloString.shuffle
+puts ""
+puts arregloInt.include?(7)
+puts arregloString.include?("Cesar")
+puts arregloString.delete("Cesar")
